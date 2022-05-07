9485d4affa0b784088dfc243cae89d9506db8fe5
Jaziel Lopez <juan.jaziel@gmail.com>
2022-05-07 12:09:50 -0700

    argument validations

--- main.py
+++ main.py
@@ -39,14 +39,57 @@ def create_cloudwatch_events(url: str = None,  method='POST', http_error_code: i
     return events
 
 
+def validate_total_events(ctx, option, value):
+    """
+    :param ctx:
+    :param option:
+    :param value:
+    :return:
+    """
+
+    error = ""
+    if value <= 0:
+        error = "should be greater than 0"
+    if value >= 10000:
+        error = "should be less than 10000"
+
+    if len(error):
+        raise click.BadParameter(error)
+
+    click.secho(f"[INFO] valid {option.name} argument: {value}")
+
+    return value
+
+
+def validate_event_method(ctx, option, value):
+
+    """
+    :param ctx:
+    :param option:
+    :param value:
+    :return:
+    """
+
+    error = ""
+    ok_options = ['POST', 'GET', 'DELETE', 'PUT', 'PATCH']
+
+    if value not in ok_options:
+        error = f"{option.name} should be any of {'.'.join(ok_options)}"
+
+    if len(error):
+        raise click.BadParameter(error)
+
+    click.secho(f"[INFO] valid {option.name} argument: {value}")
+
+
 @click.command()
 @click.option('--profile', required=False, default='default', type=str)
 @click.option('--log-group-name', required=True, type=str)
 @click.option('--log-stream-name', required=False, type=str, default='')
 @click.option('--event-url', type=str, required=False, default='/de-eec-subscription/oauth2/token')
-@click.option('--event-method', type=str, required=False, default='POST')
+@click.option('--event-method', type=str, required=False, default='POST', callback=validate_event_method)
 @click.option('--event-status-code', type=int, required=False, default=500)
-@click.option('--total-events', type=int, required=False, default=10)
+@click.option('--total-events', type=int, required=False, callback=validate_total_events, default=10)
 def main(profile=None, log_group_name=None, log_stream_name=None, event_url=None, event_method=None,
          event_status_code=None, total_events=None):
 
--- src/Logs.py
+++ src/Logs.py
@@ -6,6 +6,13 @@ from boto3.session import Session
 class CloudwatchLogs:
 
     """
+    Note: per boto3 spec you should be aware of the following limitations:
+        - The maximum batch size is 1,048,576 bytes. This size is calculated as the sum of all event messages in UTF-8,
+        - plus 26 bytes for each log event.
+        - None of the log events in the batch can be more than 2 hours in the future.
+        - None of the log events in the batch can be older than 14 days or older than the retention period of the log group.
+        - A batch of log events in a single request cannot span more than 24 hours. Otherwise, the operation fails.
+        - The maximum number of log events in a batch is 10,000.
     """
 
     _client: Any = None
